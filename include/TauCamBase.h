ce58863b2e95c56412e9bd35a66904fa803d6f03
rynskyi <rynskyi@gmail.com>
2022-11-25 15:38:43 +0200

    add read function

--- include/TauCamBase.h
+++ include/TauCamBase.h
@@ -4,23 +4,48 @@
 #define LSB(X) X & 0xFF
 #define MSB(X) X >> 8
 
+#define TAU_RES_BUF_LEN 128
+
+// TAU Success/Error responses
+#define TAU_OK                          0x00
+#define TAU_RANGE_ERROR                 0x03
+#define TAU_CHECKSUM_ERROR              0x04
+#define TAU_UNDEFINED_PROCESS_ERROR     0x05
+#define TAU_UNDEFINED_FUNCTION_ERROR    0x06
+#define TAU_TIMEOUT_ERROR               0x07
+#define TAU_BYTE_COUNT_ERROR            0x09
+#define TAU_FEATURE_NOT_ENABLED         0x0A
+
+// Tau Castom Error response
+#define TAU_RES_ERR_LEN                 0xF1
+#define TAU_RES_ERR_INVALID             0xF1
+#define TAU_RES_ERR_CRC                 0xF3
+#define TAU_RES_ERR_UDEFINED            0xFF
+
+
 class TauCamBase {
 private:
     // properies
     void (*serialWriteByte)(uint8_t);
+    void (*serialDelay)(uint32_t);
     uint8_t (*serialReadByte)();
     uint32_t (*serialAvailableBytes)();
+    uint8_t buffer[TAU_RES_BUF_LEN];
+
     // methods
-    void read();
-    void write(const void *pBuf, uint32_t len);
     uint16_t crc16(uint8_t *pBlock, uint16_t len);
+    uint32_t available();
+    uint32_t read();
+    void write(const void *pBuf, uint32_t len);
 
 protected:
-    void sendCommand(uint8_t cmd, const void *pArgs, uint16_t argsLen);
+    uint8_t sendCommand(uint8_t cmd, const void *pArgs, uint16_t argsLen);
+    uint8_t readResponse();
 public:
     TauCamBase(
         void (*writeByte)(uint8_t),
         uint8_t (*readByte)() = NULL,
-        uint32_t (*availableBytes)() = NULL
+        uint32_t (*availableBytes)() = NULL,
+        void (*delayMs)(uint32_t) = NULL
     );
 };
--- src/TauCamBase.cpp
+++ src/TauCamBase.cpp
@@ -3,25 +3,55 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+// only for debug
+void printHexBuf(const char *title, const void *pBuf, uint32_t len) {
+    printf("%s\n", title);
+    for (uint32_t i = 0; i < len; i++) {
+        uint8_t byte = *((uint8_t*)pBuf+i);
+        printf("0x%02X ", byte);
+    }
+    printf("\n\n");
+}
+
 TauCamBase::TauCamBase(
     void (*writeByte)(uint8_t),
     uint8_t (*readByte)(),
-    uint32_t (*availableBytes)()
+    uint32_t (*availableBytes)(),
+    void (*delayMs)(uint32_t)
 ) {
     TauCamBase::serialWriteByte = writeByte;
     TauCamBase::serialReadByte = readByte;
     TauCamBase::serialAvailableBytes = availableBytes;
+    TauCamBase::serialDelay = delayMs;
 };
 
 void TauCamBase::write(const void *pBuf, uint32_t len) {
-    // print to stdout for dev
-    printf("Send HEX\n");
+    // dev debug
+    printHexBuf("Send HEX", pBuf, len);
+    // write len bytes to serial
     for (uint32_t i = 0; i < len; i++) {
         uint8_t byte = *((uint8_t*)pBuf+i);
-        printf("0x%02X ", byte);
         TauCamBase::serialWriteByte(byte);
     }
-    printf("\n\n");
+}
+
+uint32_t TauCamBase::read() {
+    // clear buffer
+    memset(TauCamBase::buffer, 0, TAU_RES_BUF_LEN);
+    // read from serial to buffer
+    uint32_t len = 0;
+    while (TauCamBase::available() && len < TAU_RES_BUF_LEN) {
+        TauCamBase::buffer[len] = TauCamBase::read();
+        len++;
+    }
+    // dev debug
+    printHexBuf("Read HEX", TauCamBase::buffer, len);
+    return len;
+}
+
+uint32_t TauCamBase::available() {
+    return TauCamBase::serialAvailableBytes();
 }
 
 // calc CRC16 checksum. Polynomial = 0x1021, Initial Value = 0x0
@@ -37,7 +67,7 @@ uint16_t TauCamBase::crc16(uint8_t *pBlock, uint16_t len) {
 }
 
 // Build package structure and write to camera
-void TauCamBase::sendCommand(uint8_t cmd, const void *pArgs = NULL, uint16_t argsLen = 0) {
+uint8_t TauCamBase::sendCommand(uint8_t cmd, const void *pArgs = NULL, uint16_t argsLen = 0) {
 
     // allocate memory size = (header = 8b) + (arguments = Nb) + (CRC2 = 2b)
     uint16_t packLen = 8 + argsLen + (argsLen ? 2 : 0);
@@ -65,4 +95,18 @@ void TauCamBase::sendCommand(uint8_t cmd, const void *pArgs = NULL, uint16_t arg
     // write package
     this->write(pPack, packLen);
     free(pPack);
+    TauCamBase::serialDelay(50);
+    return TauCamBase::readResponse();
 };
+
+uint8_t TauCamBase::readResponse() {
+    uint32_t len = TauCamBase::read();
+    uint8_t *pBuf = TauCamBase::buffer;
+    
+    // check len and first byte code
+    if (len == 0 || len < 8) return TAU_RES_ERR_LEN;
+    if (pBuf[0] != 0x6E) return TAU_RES_ERR_INVALID;
+
+    // return response status byte
+    return pBuf[1];
+}
